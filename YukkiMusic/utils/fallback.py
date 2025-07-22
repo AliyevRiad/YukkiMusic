092e20a7755a1010d8a8a8793d571c2383f3f781
AliyevRiad <tagiyevilkan70@gmail.com>
2025-07-22 12:22:22 +0400

    Update fallback.py
    Signed-off-by: AliyevRiad <tagiyevilkan70@gmail.com>

--- YukkiMusic/utils/fallback.py
+++ YukkiMusic/utils/fallback.py
@@ -12,7 +12,8 @@ from YukkiMusic.platforms import saavn
 
 
 async def download(title, video):
-    raise ValueError("Failed to download song from youtube")
-    video = None
-    path, details = await saavn.download(title)
-    return path, details, video
+    try:
+        path, details = await saavn.download(title)
+        return path, details, video
+    except Exception as e:
+        raise ValueError(f"Fallback failed: {str(e)}")
